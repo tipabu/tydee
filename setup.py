9fe36f792b8bee19ce5abf42b9ebe84d31f1dea4
Tim Burke <tim.burke@gmail.com>
2018-02-07 19:19:56 -0800

    There's no escaping ownership :P

--- setup.py
+++ setup.py
@@ -4,4 +4,8 @@ setuptools.setup(
     name='tydee',
     version='0.1.0',
     packages=['tydee'],
+
+    author='Tim Burke',
+    author_email='tim.burke@gmail.com',
+    description='A tiny DNS server',
 )
