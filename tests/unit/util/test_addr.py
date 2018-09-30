7bc9a6eacebc2ca1588678ff9c0972bb6227d6a3
Tim Burke <tim.burke@gmail.com>
2018-09-29 22:15:39 -0700

    Move IP address classes to a util module
    While we're at it, stop having IPv4Address inherit from IPv6Address;
    instead have them each inherit from a BaseIPAddress class.

---
+++ tests/unit/util/test_addr.py
@@ -0,0 +1,75 @@
+from __future__ import unicode_literals
+import unittest
+
+import tydee.util.addr
+
+
+class TestIPAddresses(unittest.TestCase):
+    def test_ipv4(self):
+        a = tydee.util.addr.IPv4Address('127.0.0.1')
+        self.assertEqual(a.__bytes__(), b'\x7f\x00\x00\x01')
+        self.assertEqual(str(a), '127.0.0.1')
+        self.assertEqual(int(a), 0x7f000001)
+
+        a = tydee.util.addr.IPv4Address(0x7f000001)
+        self.assertEqual(a.__bytes__(), b'\x7f\x00\x00\x01')
+        self.assertEqual(str(a), '127.0.0.1')
+        self.assertEqual(int(a), 0x7f000001)
+
+        a = tydee.util.addr.IPv4Address(b'\x0a\x00\x00\x00')
+        self.assertEqual(a.__bytes__(), b'\x0a\x00\x00\x00')
+        self.assertEqual(str(a), '10.0.0.0')
+        self.assertEqual(str(tydee.util.addr.IPv4Address(int(a))), '10.0.0.0')
+        self.assertEqual(str(a | ((1 << 16) - 1)), '10.0.255.255')
+
+    def test_ipv6(self):
+        a = tydee.util.addr.IPv6Address('2001:db8::')
+        self.assertEqual(a.__bytes__(), b'\x20\x01\x0d\xb8' + 12 * b'\x00')
+        self.assertEqual(str(a), '2001:db8::')
+        self.assertEqual(int(a), 0x20010db8000000000000000000000000)
+
+        a = tydee.util.addr.IPv6Address(0x20010db8000000000000000000000000)
+        self.assertEqual(a.__bytes__(), b'\x20\x01\x0d\xb8' + 12 * b'\x00')
+        self.assertEqual(str(a), '2001:db8::')
+        self.assertEqual(int(a), 0x20010db8000000000000000000000000)
+
+        a = tydee.util.addr.IPv6Address(b'\x20\x01\x0d\xb8' + 12 * b'\x00')
+        self.assertEqual(a.__bytes__(), b'\x20\x01\x0d\xb8' + 12 * b'\x00')
+        self.assertEqual(str(a), '2001:db8::')
+        self.assertEqual(int(a), 0x20010db8000000000000000000000000)
+
+        addr = '::ffff:127.0.0.1'
+        self.assertEqual(len(addr), tydee.util.addr.IPv6Address.WIDTH)
+        a = tydee.util.addr.IPv6Address(addr)
+        self.assertEqual(a.__bytes__(),
+                         b'\x00' * 10 + b'\xff' * 2 + b'\x7f\x00\x00\x01')
+        self.assertEqual(str(a), '::ffff:127.0.0.1')
+        self.assertEqual(int(a), 0x00000000000000000000ffff7f000001)
+
+    def test_comparisons(self):
+        addrs = [tydee.util.addr.IPv4Address(x) for x in (
+            '127.0.0.0', '127.0.0.1', '127.0.0.255')]
+
+        self.assertTrue(addrs[0] <= addrs[0])
+        self.assertTrue(addrs[0] >= addrs[0])
+        self.assertFalse(addrs[0] < addrs[0])
+        self.assertFalse(addrs[0] > addrs[0])
+
+        self.assertTrue(addrs[0] < addrs[1])
+        self.assertFalse(addrs[2] < addrs[1])
+        self.assertFalse(addrs[0] > addrs[1])
+        self.assertTrue(addrs[2] > addrs[1])
+
+        self.assertTrue(addrs[0] < addrs[1] < addrs[2])
+        self.assertTrue(addrs[2] > addrs[1] > addrs[0])
+
+        a4 = tydee.util.addr.IPv4Address('127.0.0.1')
+        a6 = tydee.util.addr.IPv6Address('::1')
+        with self.assertRaises(TypeError):
+            a4 < a6
+        with self.assertRaises(TypeError):
+            a4 > a6
+        with self.assertRaises(TypeError):
+            a6 < a4
+        with self.assertRaises(TypeError):
+            a6 > a4
---
+++ tydee/util/addr.py
@@ -0,0 +1,98 @@
+from __future__ import print_function, unicode_literals
+import socket
+import struct
+
+try:
+    int_types = (int, long)
+except NameError:
+    int_types = (int, )
+
+
+class BaseIPAddress(object):
+    __slots__ = ('_packed',)
+
+    def __init__(self, address):
+        if isinstance(address, int_types):
+            self._packed = b''.join(
+                struct.pack('!I', (address >> (
+                    (self.WIDTH - 4 - x) * 8)) & 0xffffffff)
+                for x in range(0, self.WIDTH, 4))
+            return
+
+        try:
+            self._packed = socket.inet_pton(self.AF, address)
+        except (socket.error, TypeError):
+            if isinstance(address, bytes) and len(address) == self.WIDTH:
+                self._packed = address
+            else:
+                raise ValueError('Bad IP address %r' % (address, ))
+
+    @property
+    def WIDTH(self):
+        raise NotImplemented
+
+    @property
+    def AF(self):
+        raise NotImplemented
+
+    def __bytes__(self):
+        return self._packed
+
+    def __str__(self):
+        return socket.inet_ntop(self.AF, self._packed)
+
+    def __repr__(self):
+        return '%s(%r)' % (self.__class__.__name__, str(self))
+
+    def __int__(self):
+        result = 0
+        for x in range(0, self.WIDTH, 4):
+            result = (result << 32) + struct.unpack(
+                '!I', self._packed[x:x + 4])[0]
+        return result
+
+    def __and__(self, other):
+        return type(self)(int(self) & other)
+
+    def __or__(self, other):
+        return type(self)(int(self) | other)
+
+    def __bool__(self):
+        return any(struct.unpack('B' * len(self._packed), self._packed))
+
+    __nonzero__ = __bool__
+
+    def __eq__(self, other):
+        if not isinstance(other, type(self)):
+            return False
+        return self._packed == other._packed
+
+    def __lt__(self, other):
+        if not isinstance(other, type(self)):
+            raise TypeError('unorderable types')
+        return self._packed < other._packed
+
+    def __le__(self, other):
+        if not isinstance(other, type(self)):
+            raise TypeError('unorderable types')
+        return self._packed <= other._packed
+
+    def __gt__(self, other):
+        if not isinstance(other, type(self)):
+            raise TypeError('unorderable types')
+        return self._packed > other._packed
+
+    def __ge__(self, other):
+        if not isinstance(other, type(self)):
+            raise TypeError('unorderable types')
+        return self._packed >= other._packed
+
+
+class IPv4Address(BaseIPAddress):
+    WIDTH = 4
+    AF = socket.AF_INET
+
+
+class IPv6Address(BaseIPAddress):
+    WIDTH = 16
+    AF = socket.AF_INET6
